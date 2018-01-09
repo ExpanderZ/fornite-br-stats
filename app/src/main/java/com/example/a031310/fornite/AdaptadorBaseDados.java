51b9322e54ffb68692d017499f0ca5fbf405cef7
Nuno Botelho <botelhonuno92@gmail.com>
2018-01-09 20:54:26 +0000

    a

---
+++ app/src/main/java/com/example/a031310/fornite/AdaptadorBaseDados.java
@@ -0,0 +1,8 @@
+package com.example.a031310.fornite;
+
+/**
+ * Created by A031310 on 09/01/2018.
+ */
+
+public class AdaptadorBaseDados {
+}
---
+++ app/src/main/java/com/example/a031310/fornite/AjudaUsoBaseDados.java
@@ -0,0 +1,31 @@
+package com.example.a031310.fornite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by A031310 on 09/01/2018.
+ */
+
+    public class AjudaUsoBaseDados extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "base-dados.db";
+        private static final int VERSION = 1;
+        public AjudaUsoBaseDados(Context context) {
+            super(context, DATABASE_NAME, null, VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String s = "CREATE TABLE players(_id integer primary key autoincrement, playerNickname varchar(40), name varchar(40))";
+            db.execSQL(s);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //db.execSQL("DROP TABLE IF EXISTS players");
+            //onCreate(db);
+        }
+    }
+
