87e8e81f394c04052ac90daa27cb27878c36048d
Nuno Botelho <botelhonuno92@gmail.com>
2018-01-09 20:49:31 +0000

    A

--- app/src/main/java/com/example/a031310/fornite/MainActivity.java
+++ app/src/main/java/com/example/a031310/fornite/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.a031310.fornite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    protected TextView textView, textView2;
+    protected EditText editText;
+    protected Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        editText = (EditText) findViewById(R.id.editText;
+        button = (Button) findViewById(R.id.button);
     }
 }
